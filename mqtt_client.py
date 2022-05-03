f8fc088d477c82c05b472677f55ae153361aa164
kenny0707 <100082366+kenny0707@users.noreply.github.com>
2022-05-03 20:34:05 +0800

    Update mqtt_client.py

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,4 +1,5 @@
 from itertools import count
+from tkinter import CENTER
 from urllib.error import ContentTooShortError
 import paho.mqtt.client as paho
 import time
@@ -27,11 +28,16 @@ flag = 0
 
 cnttt = 0
 
+global cnter
+cnter = 0
+
 # Callbacks
 def on_connect(self, mosq, obj, rc):
     print("Connected rc: " + str(rc))
 
 def on_message(mosq, obj, msg):
+    global cnter
+    cnter = 0
     global cnttt
     global flag
     message = str(msg.payload)
@@ -42,8 +48,6 @@ def on_message(mosq, obj, msg):
         y[cnttt] = int(data[2])
         z[cnttt] = int(data[3])
         cnttt += 1
-    if cnttt == 49:
-        flag = 1
 
 def on_subscribe(mosq, obj, mid, granted_qos):
     print("Subscribed OK")
@@ -69,9 +73,25 @@ while num != 5:
     time.sleep(1.5)
     num += 1
 
-while flag != 1:
+while cnter != 2:
     mqttc.loop()
     time.sleep(0.1)
+    cnter += 1
+
+fig, ax = plt.subplots(3, 1)
+ax[0].plot(t,x)
+ax[0].set_xlabel('Time')
+ax[0].set_ylabel('x Amplitude')
+ax[1].plot(t,y)
+ax[1].set_xlabel('Time')
+ax[1].set_ylabel('y Amplitude')
+ax[2].plot(t,z)
+ax[2].set_xlabel('Time')
+ax[2].set_ylabel('z Amplitude')
+plt.show()
+
+# Loop forever, receiving messages
+mqttc.loop_forever()
 
 fig, ax = plt.subplots(3, 1)
 ax[0].plot(t,x)
