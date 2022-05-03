33fe107c17f79471097f0d3e0ce4ae87bb9b1a39
kenny0707 <100082366+kenny0707@users.noreply.github.com>
2022-05-03 17:30:04 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,28 +3,50 @@
 #include "MQTTmbed.h"
 #include "MQTTClient.h"
 #include "stm32l475e_iot01_accelero.h"
+
 #include "erpc_simple_server.h"
 #include "erpc_basic_codec.h"
 #include "erpc_crc16.h"
 #include "UARTTransport.h"
 #include "DynamicMessageBufferFactory.h"
 #include "controller_server.h"
-using namespace std::chrono;
-
-int16_t pDataXYZ[3] = {0};
-bool measure = false;
-
-EventQueue queue;
-Thread t;
+// GLOBAL VARIABLES
 WiFiInterface *wifi;
 const char* topic = "Mbed";
 
-void stop() {
-    measure = false;
+int16_t pDataXYZ[3] = {0};
+bool genXYZ = false;
+
+Thread mqtt_thread;
+EventQueue mqtt_queue;
+
+void start()
+{
+    genXYZ = true;
 }
 
-void start() {
-    measure = true;
+void stop()
+{
+    genXYZ = false;
+}
+
+void publish_message(MQTT::Client<MQTTNetwork, Countdown>* client) {
+        while(genXYZ)
+        {
+            MQTT::Message message;
+            char buff[100];
+            BSP_ACCELERO_AccGetXYZ(pDataXYZ);
+            sprintf(buff, " %d %d %d \n", pDataXYZ[0], pDataXYZ[1], pDataXYZ[2]);
+            message.qos = MQTT::QOS0;
+            message.retained = false;
+            message.dup = false;
+            message.payload = (void*) buff;
+            message.payloadlen = strlen(buff) + 1;
+            int rc = client->publish(topic, message);
+
+            printf("%s\r\n", buff);
+            ThisThread::sleep_for(100ms);
+        }
 }
 
 ep::UARTTransport uart_transport(D1, D0, 9600);
@@ -32,64 +54,69 @@ ep::DynamicMessageBufferFactory dynamic_mbf;
 erpc::BasicCodecFactory basic_cf;
 erpc::Crc16 crc16;
 erpc::SimpleServer rpc_server;
+
+/** LED service */
 SwitchService_service control_service;
 
-void publish_message(MQTT::Client<MQTTNetwork, Countdown>* client) {
-    while (measure) {
-        MQTT::Message message;
-        char buff[100];
-        BSP_ACCELERO_AccGetXYZ(pDataXYZ);
-        sprintf(buff, " %d %d %d ", pDataXYZ[0], pDataXYZ[1], pDataXYZ[2]);
-        message.qos = MQTT::QOS0;
-        message.retained = false;
-        message.dup = false;
-        message.payload = (void*) buff;
-        message.payloadlen = strlen(buff) + 1;
-        int rc = client->publish(topic, message);
-        printf("%s\r\n", buff);
-        ThisThread::sleep_for(100ms);
-    }
-}
+int main() {
 
-int main() {  
     BSP_ACCELERO_Init();
-    t.start(callback(&queue, &EventQueue::dispatch_forever));
+   
     wifi = WiFiInterface::get_default_instance();
     if (!wifi) {
             printf("ERROR: No WiFiInterface found.\r\n");
             return -1;
     }
+
+
     printf("\nConnecting to %s...\r\n", MBED_CONF_APP_WIFI_SSID);
     int ret = wifi->connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
     if (ret != 0) {
             printf("\nConnection error: %d\r\n", ret);
             return -1;
     }
-    const char* host = "172.20.10.6";
+
+
+    NetworkInterface* net = wifi;
+    MQTTNetwork mqttNetwork(net);
+    MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
+
+    //TODO: revise host to your IP
+    const char* host = "172.20.10.6
+";
     const int port=1883;
     printf("Connecting to TCP network...\r\n");
     printf("address is %s/%d\r\n", host, port);
-    NetworkInterface* net = wifi;
-    MQTTNetwork mqttNetwork(net);
-    MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork); 
-    int rc = mqttNetwork.connect(host, port);
+
+    int rc = mqttNetwork.connect(host, port);//(host, 1883);
     if (rc != 0) {
             printf("Connection error.");
             return -1;
     }
     printf("Successfully connected!\r\n");
-    queue.call_every(100ms, &publish_message, &client);
+
     MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
     data.MQTTVersion = 3;
     data.clientID.cstring = "Mbed";
+
     if ((rc = client.connect(data)) != 0){
             printf("Fail to connect MQTT\r\n");
     }
+
+    mqtt_thread.start(callback(&mqtt_queue, &EventQueue::dispatch_forever));
+    mqtt_queue.call_every(100ms, &publish_message, &client);
+
     uart_transport.setCrc16(&crc16);
     rpc_server.setTransport(&uart_transport);
     rpc_server.setCodecFactory(&basic_cf);
     rpc_server.setMessageBufferFactory(&dynamic_mbf);
+
+    // Add the led service to the server
     rpc_server.addService(&control_service);
+
+    // Run the server. This should never exit
+
     rpc_server.run();
+
     return 0;
 }
